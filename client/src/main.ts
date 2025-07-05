116bb8ec63cc98621e4a41b1aaec82fa907511e2
jack <Paulo Tada>
2025-07-05 14:38:56 -0300

    feat: adding css lib vuestic

--- client/src/main.ts
+++ client/src/main.ts
@@ -2,10 +2,14 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import { createVuestic } from "vuestic-ui"
 import App from './App.vue'
+
+import "vuestic-ui/css"
 
 const app = createApp(App)
 
 app.use(createPinia())
+app.use(createVuestic())
 
 app.mount('#app')
